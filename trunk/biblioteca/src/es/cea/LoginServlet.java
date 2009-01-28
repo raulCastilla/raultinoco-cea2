7a617d3c5010530398ae8fdda5adedba4a56bae4
joseemiliocampos <joseemiliocampos@e39e24ec-e24b-11dd-bce9-bf72c0239d3a>
2009-01-28 16:48:06 +0000

    Pantalla Login

--- trunk/biblioteca/src/es/cea/LoginServlet.java
+++ trunk/biblioteca/src/es/cea/LoginServlet.java
@@ -1,6 +1,7 @@
 package es.cea;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -13,6 +14,22 @@ public class LoginServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		List<Usuario> usuarios = (List<Usuario>)request.getSession().getServletContext().getAttribute(AtributosConstantes.usuarios.toString());
+		
+		response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        
+        HtmlUtilities html=new HtmlUtilities("Login");
+        
+		writer.println(html.head);
+		writer.println(html.cuerpo);
+
+        writer.println("<form action='./Login'method='post'>" +
+                "Mail:<input type='text' name='mail' /><br>" +
+                "Clave: <input type='password' name='clave' /><br>" +
+                "<input type='submit' value='Log In'/></form>");
+        
+		writer.println(html.fin);
+
 		for(Usuario tmp:usuarios){
 			if(tmp.mail.equals(request.getParameter("mail"))&&tmp.clave.equals(request.getParameter("clave"))){
 				request.getSession().setAttribute(AtributosConstantes.usuarioRegistrado.toString(), tmp);

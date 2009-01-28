3fc8326c192d3c80b36e4eb43f1a8d95ef0b1623
ul2mac <ul2mac@e39e24ec-e24b-11dd-bce9-bf72c0239d3a>
2009-01-28 16:35:45 +0000

    Clase con la clave y el nombre del administrador. Se han declarado como static final para poder ser accesibles desde cualquier parte del código sin necesidad de instanciarla.

---
+++ biblioteca/src/es/cea/Administrador.java
@@ -0,0 +1,9 @@
+package es.cea;
+
+import java.io.Serializable;
+
+public class Administrador implements Serializable{
+	private static final long serialVersionUID = 1L;
+	static final String nombre="admin";
+	static final String clave="admin";
+}

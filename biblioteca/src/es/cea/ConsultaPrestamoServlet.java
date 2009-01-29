70cdabe9822516ea5e4b2f5f68834c2f1c577123
hamlet438 <hamlet438@e39e24ec-e24b-11dd-bce9-bf72c0239d3a>
2009-01-29 23:23:42 +0000

    ConsultaPrestamoServlet (sin link de salida)

---
+++ biblioteca/src/es/cea/ConsultaPrestamoServlet.java
@@ -0,0 +1,51 @@
+package es.cea;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ConsultaPrestamoServlet extends HttpServlet {
+	
+		private static final long serialVersionUID = 1L;
+		
+		
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+                response.setContentType("text/html");
+                PrintWriter writer = response.getWriter();
+                
+                HtmlUtilities html = new HtmlUtilities("Prestamos");
+                writer.println(html.head);
+                writer.println(html.cuerpo);
+                
+                Prestamo[] prestamos = (Prestamo[]) request.getSession().getAttribute(AtributosConstantes.prestamosUsuario.toString());
+        
+                writer.println("<table width='100%' border='3'>");
+                writer.println("<tr><td><b>REFERENCIA</b></td>" +
+                               "<td><b>TITULO</b></td>" +
+                               "<td><b>INICIO DEL PRESTAMO</b></td>" + 
+                               "<td><b>FIN DEL PRESTAMO</b></td>" +
+                               "</tr>");
+                
+                for (Prestamo prest : prestamos) {
+                        writer.println("<tr><td>"+prest.libro.referencia+"</td>" +
+                                       "<td>"+prest.libro.titulo+"</td>" +
+                                       "<td>"+prest.fechaInicio+"</td>" +
+                                       "<td>"+prest.fechaFin+"</td></tr>");       
+                }
+                
+                writer.println("</table>");
+                writer.println(html.fin);
+                writer.close();
+                
+                //Requiere enlace de salida
+        
+        }
+
+        protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+                doGet(request, response);
+        }
+}

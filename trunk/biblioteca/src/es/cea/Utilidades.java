951aa4812545f5fa33b31e02882e4f816f77c05f
ul2mac <ul2mac@e39e24ec-e24b-11dd-bce9-bf72c0239d3a>
2009-01-30 14:48:38 +0000

    Utilidades para guardar la referencia del pedido

---
+++ trunk/biblioteca/src/es/cea/Utilidades.java
@@ -0,0 +1,14 @@
+package es.cea;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class Utilidades {
+	public static String obtenerURLActual(HttpServletRequest httpRequest) {
+         
+         String peticionActualDeUsuario=httpRequest.getRequestURI().replace("/biblioteca", "")+"?"+httpRequest.getQueryString();
+         System.out.println("petition actual con parametros:"+peticionActualDeUsuario);
+
+         return peticionActualDeUsuario;
+	 }
+
+}

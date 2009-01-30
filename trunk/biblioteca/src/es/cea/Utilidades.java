552b907c4a21dc7b22b3d5d1dbbf06f9fdd6d24d
ul2mac <ul2mac@e39e24ec-e24b-11dd-bce9-bf72c0239d3a>
2009-01-30 21:51:17 +0000

    Clase utilidades

--- trunk/biblioteca/src/es/cea/Utilidades.java
+++ trunk/biblioteca/src/es/cea/Utilidades.java
@@ -4,11 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 
 public class Utilidades {
 	public static String obtenerURLActual(HttpServletRequest httpRequest) {
-         
-         String peticionActualDeUsuario=httpRequest.getRequestURI().replace("/biblioteca", "")+"?"+httpRequest.getQueryString();
-         System.out.println("petition actual con parametros:"+peticionActualDeUsuario);
-
-         return peticionActualDeUsuario;
-	 }
-
+		String peticionActualDeUsuario=httpRequest.getRequestURI().replace("/biblioteca", "")+"?"+httpRequest.getQueryString();
+		System.out.println("petition actual con parametros:"+peticionActualDeUsuario);
+		return peticionActualDeUsuario;
+	}
 }

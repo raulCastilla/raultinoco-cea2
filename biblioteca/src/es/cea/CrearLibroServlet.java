b4aeb08c3e41ca29af8297ef369d01da586ffa92
ul2mac <ul2mac@e39e24ec-e24b-11dd-bce9-bf72c0239d3a>
2009-01-30 13:09:27 +0000

    Se ha añadido el formulario para añadir un nuevo libro

--- biblioteca/src/es/cea/CrearLibroServlet.java
+++ biblioteca/src/es/cea/CrearLibroServlet.java
@@ -1,6 +1,7 @@
 package es.cea;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -16,29 +17,53 @@ public class CrearLibroServlet extends HttpServlet {
 		List<Libro> libro=(List<Libro>)request.getSession().getServletContext().getAttribute(AtributosConstantes.libros.toString());
 		List<Autor> autor=(List<Autor>)request.getSession().getServletContext().getAttribute(AtributosConstantes.autores.toString());
 		
-		Autor aut = new Autor(request.getParameter("autor"));
-		Genero gen = new Genero(request.getParameter("genero"));
-		Libro lib= new Libro("RFC-1314","El libro de la selva",gen,aut);
+		response.setContentType("text/html");
+		PrintWriter pw = response.getWriter();
+		HtmlUtilities html = new HtmlUtilities("A–adir libro");
+		pw.println(html.head);
+		pw.println(html.cuerpo);
 		
-		if(!genero.contains(gen)){ 
-			gen.libros.add(lib);
-			genero.add(gen);
+		if(request.getParameter("crear")!=null){
+			Autor aut = new Autor(request.getParameter("autor"));
+			Genero gen = new Genero(request.getParameter("genero"));
+			Libro lib= new Libro(request.getParameter("referencia"),request.getParameter("titulo"),gen,aut,request.getParameter("fecha"));
+			
+			/*genero.add(gen);
+			autor.add(aut);*/
+			libro.add(lib);
+			if(!genero.contains(gen)){ 
+				gen.libros.add(lib);
+				genero.add(gen);
+			}
+			else{
+				Genero g=null;
+				for(Genero tmp:genero) if(tmp.nombre.equals(request.getParameter("genero"))) g=tmp;
+				g.libros.add(lib);
+			}
+			if(!autor.contains(aut)){ 
+				aut.libros.add(lib);
+				autor.add(aut);
+			}
+			else{
+				Autor a=null;
+				for(Autor tmp:autor) if(tmp.nombre.equals(request.getParameter("autor"))) a=tmp;
+				a.libros.add(lib);
+			}
+			pw.println("LIBRO CREADO<br>");
+			pw.println("<a href='./crear'>Crear otro</a>");
 		}
 		else{
-			Genero g=null;
-			for(Genero tmp:genero) if(tmp.nombre.equals(request.getParameter("genero"))) g=tmp;
-			g.libros.add(lib);
+			pw.println("<form action='./crear?crear=true' method='post'>"+
+					"<table>"+
+					"<tr><td>REFERENCIA:</td><td><input type='text' name='referencia' /></td></tr>"+
+					"<tr><td>TITULO:</td><td><input type='text' name='titulo' /></td></tr>"+
+					"<tr><td>AUTOR:</td><td><input type='text' name='autor' /></td></tr>"+
+					"<tr><td>GENERO</td><td><input type='text' name='genero' /></td></tr>"+
+					"<tr><td>FECHA</td><td><input type='text' name='fecha' /></td></tr>"+
+					"<tr><td><input type='submit' value='Crear' /></td><td></td></tr>"+
+					"</table></form>");
 		}
-		if(!autor.contains(aut)){ 
-			aut.libros.add(lib);
-			autor.add(aut);
-		}
-		else{
-			Autor a=null;
-			for(Autor tmp:autor) if(tmp.nombre.equals(request.getParameter("autor"))) a=tmp;
-			a.libros.add(lib);
-		}
-		
+		pw.println(html.fin);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {

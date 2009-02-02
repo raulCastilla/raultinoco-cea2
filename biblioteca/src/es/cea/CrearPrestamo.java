0003826d4f6123ed44242128f3a183d7e9967cc9
ul2mac <ul2mac@e39e24ec-e24b-11dd-bce9-bf72c0239d3a>
2009-02-02 13:36:34 +0000

    nueva version

--- biblioteca/src/es/cea/CrearPrestamo.java
+++ biblioteca/src/es/cea/CrearPrestamo.java
@@ -14,13 +14,16 @@ public class CrearPrestamo extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//HtmlUtilities html = new HtmlUtilities("Prestamos");
-		//response.setContentType("text/html");
-		//response.setCharacterEncoding("UTF-8");
+		
+		response.setContentType("text/html;charset=UTF-8");
+		
 		PrintWriter pw = response.getWriter();
-		//pw.println(html.head);
-		//pw.println(html.cuerpo);
+		
+		pw.println(HtmlUtilities.head);
+		pw.println(HtmlUtilities.cuerpo);
+		pw.println(HtmlUtilities.menuUser);
 		pw.println("<h3 style='color: #FF0000'>HACER PRESTAMO</h3>");
+		
 		List<Prestamo> prestamos = (List<Prestamo>)request.getSession().getServletContext().getAttribute(AtributosConstantes.prestamos.toString());
 		Prestamo[] prestamoUsuario = (Prestamo[])request.getSession().getAttribute(AtributosConstantes.prestamosUsuario.toString());
 		List<Libro> libro = (List<Libro>)request.getSession().getServletContext().getAttribute(AtributosConstantes.libros.toString());
@@ -32,19 +35,23 @@ public class CrearPrestamo extends HttpServlet {
 		//Comprobamos primero que el libro no este prestado
 		if(user.permitido){
 			if(!lib.prestado){
-				if(numPrestamo.equals(3)){
+				if(user.numPrestamo.equals(3)){
 					pw.println("YA NO PUEDE REALIZAR M&Aacute;S PR&Eacute;STAMOS<br>");
 				}
 		
-				if(numPrestamo<3){
+				if(user.numPrestamo<3){
 					Prestamo pres = new Prestamo(lib,user);
 					lib.prestado=true;
 					prestamos.add(pres);
-					prestamoUsuario[numPrestamo]=pres;
-					numPrestamo++;
-					request.getSession().setAttribute(AtributosConstantes.numeroPrestamos.toString(), numPrestamo);
+					user.prestamos[user.numPrestamo]=pres;
+					user.numPrestamo++;
+					
+					
+					//prestamoUsuario[numPrestamo]=pres;
+					//numPrestamo++;
+					//request.getSession().setAttribute(AtributosConstantes.numeroPrestamos.toString(), numPrestamo);
 					pw.println("PR&Eacute;STAMO REALIZADO<br>");
-					pw.println("<a href='./biblioteca'>Lista de libros</a><br>");
+					
 				}
 			}
 			else{
@@ -56,14 +63,14 @@ public class CrearPrestamo extends HttpServlet {
 		}
 		pw.println("<table border='1'>");
 		pw.println("<tr><td><b>TITULO</b></td><td><b>FECHA DE PRESTAMO</b></td><td><b>FECHA DE DEVOLUCION</b></td></tr>");
-		for(Prestamo p:prestamoUsuario){
+		for(Prestamo p:user.prestamos){
 			if(p!=null){
 				pw.println("<tr><td>"+p.libro.titulo+"</td><td>"+p.fechaInicio.get(Calendar.DATE)+"/"+(p.fechaInicio.get(Calendar.MONTH)+1)+"/"+p.fechaInicio.get(Calendar.YEAR)+"</td><td>"+p.fechaFin.get(Calendar.DATE)+"/"+(p.fechaFin.get(Calendar.MONTH)+1)+"/"+p.fechaFin.get(Calendar.YEAR)+"</td></tr>");
 			}
 		}
 		pw.println("</table>");
-		pw.println("<a href='./biblioteca'>Ir a la lista de libros</a>");
-		//pw.println(html.fin);
+		pw.println(HtmlUtilities.fin);
+		pw.close();
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {

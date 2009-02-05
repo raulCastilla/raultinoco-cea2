978aa0aedd94f79c08bf6fd393bf15fe035da4d0
ul2mac <ul2mac@e39e24ec-e24b-11dd-bce9-bf72c0239d3a>
2009-02-05 18:39:56 +0000

    Inclusión de las invocaciones

--- testeo/java/test/es/cea/LocalizacionTest.java
+++ testeo/java/test/es/cea/LocalizacionTest.java
@@ -10,8 +10,14 @@ public class LocalizacionTest {
 		Localizacion local3 = new Localizacion("ref2",1);
 		Localizacion local1Bis = new Localizacion("ref1",1);
 		
-		assert(!local1.equals(local2)):"deben ser distintos";
-		assert(!local2.equals(local3)):"deben ser distintos";
-		assert(local1.equals(local1Bis)):"deben ser iguales";
+		boolean equals1=local1.equals(local2);
+		assert(!equals1):"deben ser distintos";
+		
+		
+		boolean equals2=local2.equals(local3);
+		assert(!equals2):"deben ser distintos";
+		
+		boolean equals3=local1.equals(local1Bis);
+		assert(equals3):"deben ser iguales";
 	}
 }

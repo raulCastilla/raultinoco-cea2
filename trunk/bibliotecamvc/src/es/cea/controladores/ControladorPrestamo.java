bd1db6e6e938146c99a5ef796f558b0e91da8967
alexandrareymartinez <alexandrareymartinez@e39e24ec-e24b-11dd-bce9-bf72c0239d3a>
2009-02-13 01:37:35 +0000

    Controladores usuarios

---
+++ trunk/bibliotecamvc/src/es/cea/controladores/ControladorPrestamo.java
@@ -0,0 +1,32 @@
+package es.cea.controladores;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ControladorPrestamo
+ */
+public class ControladorPrestamo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ControladorPrestamo() {
+        super();
+        
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/prestamos.jsp");
+		requestDispatcher.forward(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ trunk/bibliotecamvc/src/es/cea/controladores/ControladorSolicitud.java
@@ -0,0 +1,35 @@
+package es.cea.controladores;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ControladorSolicitud
+ */
+public class ControladorSolicitud extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+    public ControladorSolicitud() {
+        super();
+        
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/solicitarregistro.jsp");
+		requestDispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request,response);
+	}
+
+}

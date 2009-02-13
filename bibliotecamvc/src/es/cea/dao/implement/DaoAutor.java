986d0226f21e57bc0db3d04e29ac1660fea7cbe8
ul2mac <ul2mac@e39e24ec-e24b-11dd-bce9-bf72c0239d3a>
2009-02-13 12:17:43 +0000

    Incluidas excepciones en Dao y DaoAutor. Se ha creado la excepcion BibliotecaDaoExceocion

--- bibliotecamvc/src/es/cea/dao/implement/DaoAutor.java
+++ bibliotecamvc/src/es/cea/dao/implement/DaoAutor.java
@@ -2,34 +2,43 @@ package es.cea.dao.implement;
 
 import java.util.List;
 
+
 import es.cea.dao.Dao;
 import es.cea.dao.modelo.Autor;
+import es.cea.excepcion.BibliotecaDaoExcepcion;
 
 public class DaoAutor implements Dao<Autor>{
 	private List<Autor> autores = BibliotecaDB.getInstance().getAutores();
 	@Override
-	public void agregar(Autor o) {
+	public void agregar(Autor o) throws BibliotecaDaoExcepcion{
 		if(!autores.contains(o))
 			autores.add(o);
+		else
+			throw new BibliotecaDaoExcepcion();
 	}
 
 	@Override
-	public void eliminar(Autor o) {
+	public void eliminar(Autor o) throws BibliotecaDaoExcepcion{
 		if(autores.contains(o))
 			autores.remove(o);
+		else
+			throw new BibliotecaDaoExcepcion();
 	}
 
 	@Override
-	public Autor obtener(Autor o) {
+	public Autor obtener(Autor o) throws BibliotecaDaoExcepcion{
 		if(autores.contains(o))
 			return autores.get(autores.indexOf(o));
 		else
-			return null;
+			throw new BibliotecaDaoExcepcion();
 	}
 
 	@Override
-	public List<Autor> obtenerLista() {
-		return autores;
+	public List<Autor> obtenerLista() throws BibliotecaDaoExcepcion{
+		if(autores==null)
+			throw new BibliotecaDaoExcepcion();
+		else
+			return autores;
 	}
 	
 }

9fb1796b6ed17d262b2f5a8d6f17ee14f5ed356c
alexandrareymartinez <alexandrareymartinez@e39e24ec-e24b-11dd-bce9-bf72c0239d3a>
2009-02-18 19:58:18 +0000

    sorry

---
+++ trunk/bibliotecamvc/src/es/cea/tags/EtiquetaTablaDetalle.java
@@ -0,0 +1,9 @@
+package es.cea.tags;
+
+import javax.servlet.jsp.tagext.TagSupport;
+
+public class EtiquetaTablaDetalle extends TagSupport{
+
+	private static final long serialVersionUID = 1L;
+
+}

997855266cf6630c47ebb3e5e0f424f4340edcbb
hamlet438 <hamlet438@e39e24ec-e24b-11dd-bce9-bf72c0239d3a>
2009-02-19 15:57:04 +0000

    DaoSolicitudTest

--- bibliotecamvc/test/es/cea/dao/implement/DaoSolicitudTest.java
+++ bibliotecamvc/test/es/cea/dao/implement/DaoSolicitudTest.java
@@ -1,24 +1,30 @@
 package es.cea.dao.implement;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import es.cea.dao.Dao;
 import es.cea.dao.modelo.Solicitud;
 import es.cea.dao.modelo.Usuario;
+import es.cea.dao.modelo.UsuarioRegistrado;
 import es.cea.excepcion.BibliotecaDaoExcepcion;
 
 public class DaoSolicitudTest {
 	
+	@BeforeMethod
+	public void iniciarRecursos(){
+		BibliotecaDB.reset();
+	}
 	
 	@Test
 	public void agregarSolicitudLista(){
-		Dao dao = new DaoSolicitud();
-		Usuario us1 = new Usuario("us1","us1@mail.com","clave1");
-		Usuario us2 = new Usuario("us2","us2@mail.com","clave2");
+		Dao<Solicitud> dao = new DaoSolicitud();
+		UsuarioRegistrado us1 = new UsuarioRegistrado("us1","us1@mail.com","clave1");
+		UsuarioRegistrado us2 = new UsuarioRegistrado("us2","us2@mail.com","clave2");
 		
-		Solicitud sol1 = new Solicitud(us1);
-		Solicitud sol2 = new Solicitud(us2);
-		Solicitud sol3 = new Solicitud(us2);
+		Solicitud sol1 = new Solicitud(us1, null, null);
+		Solicitud sol2 = new Solicitud(us2, null, null);
+		Solicitud sol3 = new Solicitud(us2, null, null);
 		
 		
 		try{
@@ -28,7 +34,7 @@ public class DaoSolicitudTest {
 			Boolean equals1 = dao.obtenerLista().size()==2;
 			assert(equals1):"debe haber dos elementos en la lista";
 			
-			Boolean equals2 = dao.obtener(sol1).equals(dao.obtener(sol2));
+			Boolean equals2 = dao.obtener(us1).equals(dao.obtener(us2));
 			assert(!equals2):"deben ser distintos";
 			
 			dao.agregar(sol3);
@@ -45,29 +51,10 @@ public class DaoSolicitudTest {
 	}
 	
 	@Test
-	public void obtenerSolicitudLista(){
-		Dao dao = new DaoPrestamo();
-		Usuario us3 = new Usuario("us3","us3@mail.com","clave3");
-		Solicitud sol3 = new Solicitud(us3);
-		
-		try{
-			
-			dao.obtener(sol3);
-			assert(false):"no se ha lanzado la excepcion";
-		}
-		catch (BibliotecaDaoExcepcion e) {
-			// TODO: handle exception
-			System.out.println("sol3 no existe en la lista");
-			assert(true);
-		}
-		
-	}
-	
-	@Test
 	public void eliminarSolicitudLista(){
 		Dao dao = new DaoSolicitud();
-		Usuario us1 = new Usuario("us1","us1@mail.com","clave1");
-		Solicitud sol1 = new Solicitud(us1);
+		UsuarioRegistrado us1 = new UsuarioRegistrado("us1","us1@mail.com","clave1");
+		Solicitud sol1 = new Solicitud(us1, null, null);
 		
 		try{
 			dao.eliminar(sol1);

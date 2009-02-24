e6dbdf7456685b87dbc345a5b0fb545054e83e54
alexandrareymartinez <alexandrareymartinez@e39e24ec-e24b-11dd-bce9-bf72c0239d3a>
2009-02-24 19:42:46 +0000

    cambio import

--- trunk/bibliotecamvc/resources/import.sql
+++ trunk/bibliotecamvc/resources/import.sql
@@ -5,3 +5,11 @@ insert into autor(nombre_autor) values ('autor3')
 insert into autor(nombre_autor) values ('autor4')
 insert into autor(nombre_autor) values ('autor5')
 insert into autor(nombre_autor) values ('autor6')
+delete from prestamos
+insert into prestamos(referencia,mail,fechaInicio,fechaFin) values('Ref001','aa@aa.com',01/01/2009,06/01/2009)
+insert into prestamos(referencia,mail,fechaInicio,fechaFin) values('Ref002','bb@bb.com',01/01/2009,06/01/2009)
+insert into prestamos(referencia,mail,fechaInicio,fechaFin) values('Ref003','cc@cc.com',01/01/2009,06/01/2009)
+delete from genero
+insert into genero(nombre_genero) values ('genero1')
+insert into genero(nombre_genero) values ('genero2')
+insert into genero(nombre_genero) values ('genero3')
